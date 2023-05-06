88a4497e20820e3605109f00b859ee0a72fb1b55
Shmath64 <64mdavis@gmail.com>
2023-05-06 10:58:03 -0400

    Started project, py and map image

---
+++ Backend/main.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+from matplotlib import image as img
+
+plt.title("Map")
+plt.xlabel("West-East")
+plt.ylabel("North-South")
+
+
+def plotSensor(x, y):
+    plt.plot(x, y, marker='x', color="black")
+
+
+image = img.imread("Map1.png")
+plotSensor(100, 100)
+plt.imshow(image)
+plt.show()
